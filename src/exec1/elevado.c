9eff099266e95ef3f50ea309d5de0e0f6e620442
xCall <carlosfaita99@gmail.com>
2022-06-16 16:39:03 -0300

    exercicios

--- src/exec1/elevado.c
+++ src/exec1/elevado.c
@@ -2,3 +2,18 @@
 // Created by xcall on 16/06/2022.
 //
 #include <stdio.h>
+#include <math.h>
+int main() {
+    float num1, num2, total;
+
+    printf("Informe o primeiro numero:\n");
+    scanf("%f", &num1);
+
+    printf("Informe o segundo numero:\n");
+    scanf("%f", &num2);
+
+    total = pow(num1, num2);
+
+    printf("\nO %.2f elevado a %.2f e: %.2f", num1, num2, total);
+    return (0);
+}
